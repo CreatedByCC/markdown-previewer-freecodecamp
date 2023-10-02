c156bc5331546fe02ef5f8da5258d83835f7ca3c
CC <chantelh88@gmail.com>
2023-10-03 10:27:30 +1300

    Add: gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://CreatedByCC.github.io/markdown-previewer-freecodecamp',
   plugins: [react()],
 })
